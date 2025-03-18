077e90dc3010a0af00d4cbcf43cc7b194548fc50
Rafael <145466519+Fael-sf@users.noreply.github.com>
2025-03-18 15:24:07 -0300

    17/02

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoader', function(){
 const aumetaFONTEBotao = document.getElementById('aumentar-fonte');
 const diminuiFONTEBotao = document.getElementById('diminuir-fonte');
+
 let tamanhoAtualFonte = 1;
 
 aumetaFONTEBotao.addEventListener('click', function(){
  tamanhoAtualFonte += 0.1;
 document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-
 
 })
 
